7083bbc63549df421d87e21960154d01a2df44f4
Michael Naumov <michael.naumov@readify.net>
2013-06-15 22:57:46 +1000

    Fix incorrect V -> M

--- ksample-power.R
+++ ksample-power.R
@@ -230,11 +230,11 @@ PowerForMu = function(mu, distr, f, Z=1000, M=10000) {
 b=5
 k=3
 
-PowerForMu (mu=c(0,0,0),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-1/5,0,1/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-2/5,0,2/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-3/5,0,3/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-4/5,0,4/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-5/5,0,5/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-6/5,0,6/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-7/5,0,7/5),distr=rexp,Z=1000,V=10000)
+PowerForMu (mu=c(0,0,0),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-1/5,0,1/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-2/5,0,2/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-3/5,0,3/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-4/5,0,4/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-5/5,0,5/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-6/5,0,6/5),distr=rexp,Z=1000,M=10000)
+PowerForMu (mu=c(-7/5,0,7/5),distr=rexp,Z=1000,M=10000)
