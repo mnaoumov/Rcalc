74db07c9b61bff6096394d44ff3af43b80d463a4
Michael Naumov <michael.naumov@readify.net>
2013-06-19 23:27:21 +1000

    Remove PowerForMu for small values

--- new.R
+++ new.R
@@ -552,18 +552,3 @@ round(Q,4)
 L
 round(tplr,4)
 round(tpnc,4)
-
-
-
-b=5
-k=3
-
-PowerForMu (mu=c(0,0,0),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-1/5,0,1/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-2/5,0,2/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-3/5,0,3/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-4/5,0,4/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-5/5,0,5/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-6/5,0,6/5),distr=rexp,Z=1000,V=10000)
-PowerForMu (mu=c(-7/5,0,7/5),distr=rexp,Z=1000,V=10000)
-
