9b7c168628874193eea8fc048013ab49fc6a7722
Michael Naumov <michael.naumov@readify.net>
2013-06-25 23:46:32 +1000

    fix for accuracy

--- accuracy.R
+++ accuracy.R
@@ -21,12 +21,8 @@ u = (7 : 14) / 10
 M = 30
 
 #accuracy normal
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
 A = generateRandomMatrix(rnorm, b, k)
-iterations = 20000 
+iterations = 100000 
 
 Fstat = NULL
 Lstat = NULL
@@ -45,18 +41,6 @@ FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
 QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
 Q = 1 - pchisq(b * u ^ 2, k - 1)
 F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
- 
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)
-apply(FMCall,2,sd)/sqrt(100)
-
  
 tplr = NULL
 tpnc = NULL
@@ -71,22 +55,18 @@ rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,
   
 
 #accuarcy unif
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
 A = generateRandomMatrix(runif, b, k)
-iterations = 20000 
+iterations = 100000 
 
 Fstat = NULL
 Lstat = NULL
  
   for (i in 1 : iterations) {
-    B = t(apply(A, 1, sample, size = k))#apply(A, 1, sample, size = k)
-   xx = apply(B, 2, mean)#apply(B, 1, mean)
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
 SST = b * sum(xx ^ 2) 
-SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)#sum((B - xx) ^ 2)
-Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))#c(Fstat, SST/ (k - 1) / (SSE / ((k - 1) * (b - 1)) ))
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
 Lstat = c(Lstat, Lambda(A, xx[-k])$max)
    }
  
@@ -95,18 +75,6 @@ FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
 QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
 Q = 1 - pchisq(b * u ^ 2, k - 1)
 F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
- 
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)
-apply(FMCall,2,sd)/sqrt(100)
-
  
 tplr = NULL
 tpnc = NULL
@@ -121,61 +89,8 @@ rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,
  
 
 #accuarcy exp
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
 A = generateRandomMatrix(rexp, b, k)
-iterations = 20000 
-
-Fstat = NULL
-Lstat = NULL
- 
-  for (i in 1 : iterations) {
-    B = t(apply(A, 1, sample, size = k))#apply(A, 1, sample, size = k)
-   xx = apply(B, 2, mean)#apply(B, 1, mean)
-SST = b * sum(xx ^ 2) 
-SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)#sum((B - xx) ^ 2)
-Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))#c(Fstat, SST/ (k - 1) / (SSE / ((k - 1) * (b - 1)) ))
-Lstat = c(Lstat, Lambda(A, xx[-k])$max)
-   }
- 
-L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
-FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
-QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
-Q = 1 - pchisq(b * u ^ 2, k - 1)
-F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
- 
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)#
- apply(FMCall,2,sd)/sqrt(100)
-
- 
-tplr = NULL
-tpnc = NULL
-  
-for (up in u) {
-  tt = TailDistrMonteCarlo(A, up, M)
- tplr = c(tplr, tt$LR)
- tpnc = c(tpnc, tt$NC)
- }
-
-rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
-
-#accuracy exp squared
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
-A = generateRandomMatrix(rexp2, b, k)
-iterations = 20000 
+iterations = 100000 
 
 Fstat = NULL
 Lstat = NULL
@@ -195,17 +110,38 @@ QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
 Q = 1 - pchisq(b * u ^ 2, k - 1)
 F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
  
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)
-apply(FMCall,2,sd)/sqrt(100)
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
 
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
+#accuracy exp squared
+A = generateRandomMatrix(rexp2, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
  
 tplr = NULL
 tpnc = NULL
@@ -220,12 +156,8 @@ rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,
  
 
 #accuracy gamma5
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
 A = generateRandomMatrix(rgamma5, b, k)
-iterations = 20000 
+iterations = 100000 
 
 Fstat = NULL
 Lstat = NULL
@@ -245,18 +177,6 @@ QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
 Q = 1 - pchisq(b * u ^ 2, k - 1)
 F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
  
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)
-apply(FMCall,2,sd)/sqrt(100)
-
- 
 tplr = NULL
 tpnc = NULL
   
@@ -267,14 +187,10 @@ for (up in u) {
  }
 
 rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
-
+ 
 #accuracy gamma05
-FMCall=NULL
-QMCall=NULL
- 
-for (j in 1:100){
 A = generateRandomMatrix(rgamma05, b, k)
-iterations = 20000 
+iterations = 100000 
 
 Fstat = NULL
 Lstat = NULL
@@ -294,18 +210,6 @@ QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
 Q = 1 - pchisq(b * u ^ 2, k - 1)
 F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
  
-FMC
-round(F,4)
-QMC
-round(Q,4)
-FMCall=rbind(FMCall,FMC)
-QMCall=rbind(QMCall,QMC)
-} 
- 
-apply(FMCall,2,mean)
-apply(FMCall,2,sd)/sqrt(100)
-
- 
 tplr = NULL
 tpnc = NULL
   
@@ -316,3 +220,4 @@ for (up in u) {
  }
 
 rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
