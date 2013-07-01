5a7437bb0e66412358e0bbd294c26bf37cf45dfa
Michael Naumov <michael.naumov@readify.net>
2013-07-01 20:43:05 +1000

    accuracy copy

---
+++ accuracy-fixed2.R
@@ -0,0 +1,261 @@
+options(warn=2, error=quote(dump.frames("accuracy-fixed", TRUE)))
+source("PowerFunctions-fixed.R")
+
+rexp2 = function(N) {
+  rexp(N) ^ 2
+}
+
+rgamma5 = function(N) {
+  rgamma(N, 5)
+}
+
+rgamma05 = function(N) {
+  rgamma(N, 0.5)
+}
+ 
+
+
+u = (7 : 14) / 10
+M = 30
+
+b = 5
+k = 3
+
+#accuracy exp
+A = generateRandomMatrix(rexp, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+
+
+b = 10
+k = 4
+
+#accuracy normal
+A = generateRandomMatrix(rnorm, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+  
+
+#accuarcy unif
+A = generateRandomMatrix(runif, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
+
+#accuarcy exp
+A = generateRandomMatrix(rexp, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
+#accuracy exp squared
+A = generateRandomMatrix(rexp2, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
+
+#accuracy gamma5
+A = generateRandomMatrix(rgamma5, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
+#accuracy gamma05
+A = generateRandomMatrix(rgamma05, b, k)
+iterations = 100000 
+
+Fstat = NULL
+Lstat = NULL
+ 
+  for (i in 1 : iterations) {
+    B = t(apply(A, 1, sample, size = k))
+   xx = apply(B, 2, mean)
+SST = b * sum(xx ^ 2) 
+SSE = sum((B-matrix(xx,b,k,byrow=TRUE))^2)
+Fstat = c(Fstat, (SST/ (k - 1)) / (SSE / ((k - 1) * (b - 1)) ))
+Lstat = c(Lstat, Lambda(A, xx[-k])$max)
+   }
+ 
+L = apply(outer(Lstat, u ^ 2 / 2, ">"), 2, mean)
+FMC = apply(outer(Fstat, b * u ^ 2, ">"), 2, mean)
+QMC = apply(outer(Fstat*(k-1), b * u ^ 2, ">"), 2, mean)
+Q = 1 - pchisq(b * u ^ 2, k - 1)
+F = 1 - pf(b * u ^ 2 , k-1, (k-1) * (b-1))
+ 
+tplr = NULL
+tpnc = NULL
+  
+for (up in u) {
+  tt = TailDistrMonteCarlo(A, up, M)
+ tplr = c(tplr, tt$LR)
+ tpnc = c(tpnc, tt$NC)
+ }
+
+rbind(FMC, F=round(F,4), QMC, Q=round(Q,4), L , LR=round(tplr,4), NC=round(tpnc,4))
+ 
